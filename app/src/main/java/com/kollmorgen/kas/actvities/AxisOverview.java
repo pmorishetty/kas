a542aaedcab9fd7a9bfc45552e9292d0b034933f
Prashanth Morishetty <prashanth.morishetty@kollmorgen-git>
2020-10-13 13:46:22 -0700

    Add sample Axis Overview

---
+++ app/src/main/java/com/kollmorgen/kas/actvities/AxisOverview.java
@@ -0,0 +1,16 @@
+package com.kollmorgen.kas.actvities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kollmorgen.kas.R;
+
+public class AxisOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_axis_overview);
+    }
+}
